e3e6760806acbf4a012efdfa1dc5385157b6ae40
Patrick-ring-motive <60206943+Patrick-ring-motive@users.noreply.github.com>
2024-06-18 23:37:29 -0500

    Create nashorn.java

---
+++ nashorn.java
@@ -0,0 +1,13 @@
+public class nashorn{
+    public static final javax.script.ScriptEngine engine=new javax.script.ScriptEngineManager().getEngineByName("nashorn");
+	public static void main(String [] args){try{
+        System.out.println(engine.eval(
+            
+           "function run(args) {"+
+              "return eval(args)"+
+           "}"+
+           "run('"+String.join(" ",args)+"');"
+        
+    ));}catch(Exception e){System.out.println(e.getMessage());}   
+	}
+}
